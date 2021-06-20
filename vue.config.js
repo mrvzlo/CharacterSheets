a74253198a4d77eae3beb151264de2b8a735bbcd
Vladimir Andrejev <andrejevve@gmail.com>
2021-06-20 20:54:35 +0300

    Add service worker

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-    publicPath: '/DnDSheet/'
-}
+   publicPath: process.env.NODE_ENV === "production" ? "/DnDSheet/" : "/",
+   pwa: {
+      themeColor: "#333333",
+      msTileColor: "#333333",
+      appleMobileWebAppCache: "yes",
+      manifestOptions: {
+         start_url: "/DnDSheet/",
+         icons: [
+            {
+               src: "./favicon-1.png",
+               type: "image/png",
+               sizes: "256x256",
+            },
+         ],
+      },
+      workboxOptions: {
+         exclude: [/_config.yml/],
+      },
+   },
+};
