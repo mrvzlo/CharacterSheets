e9d647f6087cf0a3d66b8a7090f9aeff18e50722
Vladimir Andrejev <andrejevve@gmail.com>
2021-11-06 09:44:06 +0200

    Update version for publish

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-   appId: 'ru.charactersheets',
+   appId: 'charsheets',
    appName: 'Чарники',
    webDir: 'dist',
    bundledWebRuntime: false,
